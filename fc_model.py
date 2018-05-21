d892989e62fdeb66142cdd4adf365cd10224e8e5
imosafi <itai.mosafi@live.biu.ac.il>
2018-05-21 21:10:43 +0300

    Change model and add test printing.

--- fc_model.py
+++ fc_model.py
@@ -6,12 +6,14 @@ import torch.optim as optim
 
 
 class FCModel(nn.Module):
-    def __init__(self, in_dim, hidden_dim, out_dim):
+    def __init__(self, in_dim, hidden_dim1, hidden_dim2, out_dim):
         super(FCModel, self).__init__()
-        self.linear1 = nn.Linear(in_dim, hidden_dim)
-        self.linear2 = nn.Linear(hidden_dim, out_dim)
+        self.linear1 = nn.Linear(in_dim, hidden_dim1)
+        self.linear2 = nn.Linear(hidden_dim1, hidden_dim2)
+        self.linear3 = nn.Linear(hidden_dim2, out_dim)
 
     def forward(self, input):
-        out = F.sigmoid(self.linear1(input))
-        out = self.linear2(out)
+        out = F.relu(self.linear1(input))
+        out = F.relu(self.linear2(out))
+        out = self.linear3(out)
         return F.log_softmax(out)
--- main.py
+++ main.py
@@ -42,29 +42,32 @@ def train(epoch_num, model, train_loader, optimizer):
         loss.backward()
         optimizer.step()
     train_acc = float(100.0 * correct) / len(train_loader.sampler)
-    return train_acc, float(train_loss / len(train_loader.sampler))
+    train_loss = float(train_loss / len(train_loader.sampler))
+    print('Train set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+        train_loss, correct, len(train_loader.sampler), train_acc))
+    return train_acc, train_loss
 
 
 def validate(model, test_loader):
     model.eval()
-    test_loss = 0.0
+    val_loss = 0.0
     correct = 0.0
     for data, target in test_loader:
         data = data.reshape(-1, 28*28).to(device)
         target = target.to(device)
 
         output = model(data)
-        test_loss += F.nll_loss(output, target, size_average=False).data[0]  # sum up batch loss
+        val_loss += F.nll_loss(output, target, size_average=False).data[0]  # sum up batch loss
         pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).sum()
-    test_loss /= len(test_loader.sampler)
-    test_acc = float(100.0 * correct) / len(test_loader.sampler)
-    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
-        test_loss, correct, len(test_loader.sampler), test_acc))
-    return test_acc, float(test_loss)
+    val_loss /= len(test_loader.sampler)
+    val_acc = float(100.0 * correct) / len(test_loader.sampler)
+    print('Validation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+        val_loss, correct, len(test_loader.sampler), val_acc))
+    return val_acc, val_loss
 
 
-def get_train_valid_loaders(data_dir, batch_size):
+def get_train_validation_test_loaders(data_dir, batch_size):
     train_dataset = datasets.FashionMNIST(
         root=data_dir, train=True,
         download=True, transform=transforms.ToTensor())
@@ -72,6 +75,10 @@ def get_train_valid_loaders(data_dir, batch_size):
     valid_dataset = datasets.FashionMNIST(
         root=data_dir, train=True,
         download=True, transform=transforms.ToTensor())
+
+    test_loader = torch.utils.data.DataLoader(
+        datasets.FashionMNIST('./data', train=False, transform=transforms.ToTensor()),
+        batch_size=batch_size, shuffle=False)
 
     num_train = len(train_dataset)
     indices = list(range(num_train))
@@ -91,7 +98,7 @@ def get_train_valid_loaders(data_dir, batch_size):
         valid_dataset, batch_size=batch_size, sampler=valid_sampler,
         num_workers=1, pin_memory=True)
 
-    return (train_loader, valid_loader)
+    return (train_loader, valid_loader, test_loader)
 
 
 def save_test_predictions(model, path):
@@ -104,11 +111,11 @@ def save_test_predictions(model, path):
         f.writelines(map(lambda x: str(int(x)) + '\n', preds))
 
 
-def create_confusion_matrix(model, val_loader, categories_num=10):
+def create_confusion_matrix(model, test_loader, categories_num=10):
     confusion_martix = []
     for i in range(categories_num):
         confusion_martix.append([0] * categories_num)
-    for data, labels in val_loader:
+    for data, labels in test_loader:
         data = data.reshape(-1, 28 * 28).to(device)
         labels = labels.to(device)
 
@@ -118,30 +125,53 @@ def create_confusion_matrix(model, val_loader, categories_num=10):
         for pred, label in zip(preds, labels):
             confusion_martix[label][pred] += 1
 
-    print('Confustion matrix:')
+    print('\nConfustion matrix:\n')
     for l in range(categories_num):
         print(confusion_martix[l])
 
+
+def print_test_acc_loss(model, test_loader):
+    model.eval()
+    test_loss = 0.0
+    correct = 0.0
+    for data, target in test_loader:
+        data = data.reshape(-1, 28*28).to(device)
+        target = target.to(device)
+
+        output = model(data)
+        test_loss += F.nll_loss(output, target, size_average=False).data[0]  # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).sum()
+    test_loss /= len(test_loader.sampler)
+    test_acc = float(100.0 * correct) / len(test_loader.sampler)
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+        test_loss, correct, len(test_loader.sampler), test_acc))
+    return test_acc, test_loss
 
 def main():
     print(sys.version)
     print(torch.__version__)
     print('GPU is available' if torch.cuda.is_available() else 'GPU is not available')
 
-    train_loader, val_loader = get_train_valid_loaders('./data', batch_size=15)
+    train_loader, val_loader, test_loader = get_train_validation_test_loaders('./data', batch_size=15)
 
-    model = FCModel(784, 100, 10).to(device)
+
+
+    model = FCModel(784, 100, 50, 10).to(device)
     optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
     train_acc_list, train_loss_list, val_acc_list, val_loss_list = [], [], [], []
-    for epoch in range(100):
+    for epoch in range(10):
         train_acc, train_loss = train(epoch + 1, model, train_loader, optimizer)
         val_acc, val_loss = validate(model, val_loader)
         train_acc_list.append(train_acc)
         train_loss_list.append(train_loss)
         val_acc_list.append(val_acc)
         val_loss_list.append(val_loss)
+
+    print_test_acc_loss(model, test_loader)
+
     save_test_val_acc_loss_plots(train_acc_list, val_acc_list, train_loss_list, val_loss_list)
-    create_confusion_matrix(model, val_loader)
+    create_confusion_matrix(model, test_loader)
     save_test_predictions(model, 'data/test_x')
 
 
