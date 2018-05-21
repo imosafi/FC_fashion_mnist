5c5ae42c3c775269e0de35aab53601c83450f3b9
imosafi <itai.mosafi@live.biu.ac.il>
2018-05-21 22:44:52 +0300

    Add dropout and batch norm

--- fc_model.py
+++ fc_model.py
@@ -11,9 +11,30 @@ class FCModel(nn.Module):
         self.linear1 = nn.Linear(in_dim, hidden_dim1)
         self.linear2 = nn.Linear(hidden_dim1, hidden_dim2)
         self.linear3 = nn.Linear(hidden_dim2, out_dim)
+        self.batch_norm1 = nn.BatchNorm1d(100)
+        self.batch_norm2 = nn.BatchNorm1d(50)
 
+    #with batchnorm
     def forward(self, input):
-        out = F.relu(self.linear1(input))
-        out = F.relu(self.linear2(out))
+        out = F.relu(self.batch_norm1(self.linear1(input)))
+        out = F.relu(self.batch_norm2(self.linear2(out)))
         out = self.linear3(out)
         return F.log_softmax(out)
+
+
+    # with dropout
+    # def forward(self, input):
+    #     out = F.relu(self.linear1(input))
+    #     out = F.dropout(out, p=0.2, training=self.training)
+    #     out = F.relu(self.linear2(out))
+    #     out = F.dropout(out, p=0.2, training=self.training)
+    #     out = self.linear3(out)
+    #     return F.log_softmax(out)
+
+
+    ##regular
+    # def forward(self, input):
+    #     out = F.relu(self.linear1(input))
+    #     out = F.relu(self.linear2(out))
+    #     out = self.linear3(out)
+    #     return F.log_softmax(out)
